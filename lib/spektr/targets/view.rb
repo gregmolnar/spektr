ddb14cc1b1f90e080e702ec8ad9181773f414c82
Greg Molnar <molnargerg@gmail.com>
2021-08-08 11:23:16 +0200

    add view_path to view

--- lib/spektr/targets/view.rb
+++ lib/spektr/targets/view.rb
@@ -2,8 +2,14 @@ module Spektr
   module Targets
     class View < Base
       TEMPLATE_EXTENSIONS = /.*\.(erb|rhtml)$/
+      attr_accessor :view_path
+
       def initialize(path, content)
         @path = path
+        if match_data = path.match(/views\/(.+?)\./)
+          @view_path = match_data[1]
+        end
+
         @ast = Parser::CurrentRuby.parse(source(content))
         @name = @ast.children.first.children.last.to_s
       end
