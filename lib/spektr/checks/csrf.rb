3fa430243cc196a2ba02549926c3c1002851c76f
Greg Molnar <molnargerg@gmail.com>
2021-10-01 08:27:11 +0200

    fix csrf check

--- lib/spektr/checks/csrf.rb
+++ lib/spektr/checks/csrf.rb
@@ -6,7 +6,7 @@ module Spektr
       end
 
       def cve_2020_8186_check(calls)
-        if calls.any? && app_version_between?('0.0.0', '5.2.4.2') || app_version_between?('6.0.0', '6.0.3')
+        if app_version_between?('0.0.0', '5.2.4.2') || app_version_between?('6.0.0', '6.0.3')
           warn! @target, self, calls.first.location, "Rails #{@app.rails_version} has a vulnerability that may allow CSRF token forgery"
         end
       end
