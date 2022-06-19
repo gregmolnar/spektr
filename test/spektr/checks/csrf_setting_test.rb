282ad8af53723a45506e8a6246f0db27ba1e2bad
Greg Molnar <molnargerg@gmail.com>
2022-06-19 19:14:51 +0200

    add csrf tests

--- test/spektr/checks/csrf_setting_test.rb
+++ test/spektr/checks/csrf_setting_test.rb
@@ -1,7 +1,6 @@
-require "test_helper"
+require 'test_helper'
 
 class CsrfSettingTest < Minitest::Test
-
   def test_it_does_not_fail_when_parent_enables_protection
     application_controller = <<-CODE
       require "foobar"
@@ -14,9 +13,9 @@ class CsrfSettingTest < Minitest::Test
      end
     CODE
     app = Spektr::App.new(checks: [Spektr::Checks::CsrfSetting])
-    app.rails_version = Gem::Version.new("4.0.0")
-    app.controllers = [Spektr::Targets::Controller.new("application_controller.rb", application_controller)]
-    controller = Spektr::Targets::Controller.new("posts_controller.rb", code)
+    app.rails_version = Gem::Version.new('4.0.0')
+    app.controllers = [Spektr::Targets::Controller.new('application_controller.rb', application_controller)]
+    controller = Spektr::Targets::Controller.new('posts_controller.rb', code)
     check = Spektr::Checks::CsrfSetting.new(app, controller)
     check.run
     assert_equal 0, app.warnings.size
@@ -32,15 +31,15 @@ class CsrfSettingTest < Minitest::Test
      end
     CODE
     app = Spektr::App.new(checks: [Spektr::Checks::CsrfSetting])
-    app.rails_version = Gem::Version.new("4.0.0")
-    app.controllers = [Spektr::Targets::Controller.new("application_controller.rb", application_controller)]
-    controller = Spektr::Targets::Controller.new("posts_controller.rb", code)
+    app.rails_version = Gem::Version.new('4.0.0')
+    app.controllers = [Spektr::Targets::Controller.new('application_controller.rb', application_controller)]
+    controller = Spektr::Targets::Controller.new('posts_controller.rb', code)
     check = Spektr::Checks::CsrfSetting.new(app, controller)
     check.run
     assert_equal 1, app.warnings.size
   end
 
-  def test_it_doesnot_fails_with_multi_level_parents
+  def test_it_doesnot_fail_with_multi_level_parents
     application_controller = <<-CODE
       class ApplicationController
         protect_from_forgery
@@ -56,12 +55,37 @@ class CsrfSettingTest < Minitest::Test
       end
     CODE
     app = Spektr::App.new(checks: [Spektr::Checks::CsrfSetting])
-    app.rails_version = Gem::Version.new("4.0.0")
-    app.controllers = [Spektr::Targets::Controller.new("application_controller.rb", application_controller), Spektr::Targets::Controller.new("admin_controller.rb", admin_controller)]
-    controller = Spektr::Targets::Controller.new("posts_controller.rb", code)
+    app.rails_version = Gem::Version.new('4.0.0')
+    app.controllers = [Spektr::Targets::Controller.new('application_controller.rb', application_controller),
+                       Spektr::Targets::Controller.new('admin_controller.rb', admin_controller)]
+    controller = Spektr::Targets::Controller.new('posts_controller.rb', code)
     check = Spektr::Checks::CsrfSetting.new(app, controller)
     check.run
     assert_equal 0, app.warnings.size
+
+    code = <<-CODE
+      module Admin
+        module Settings
+          class BaseController < Admin::ApplicationController
+          end
+        end
+      end
+    CODE
+    app.controllers << Spektr::Targets::Controller.new('admin/settings/base_controller.rb', code)
+    code = <<-CODE
+      module Admin
+        module Settings
+          class CampaignsController < Admin::Settings::BaseController
+          end
+        end
+      end
+    CODE
+    controller = Spektr::Targets::Controller.new('admin/settings/campaigns_controller.rb', code)
+    app.controllers << controller
+    check = Spektr::Checks::CsrfSetting.new(app, controller)
+    check.run
+    assert_equal 0, app.warnings.size
+
     generic_controller = <<-CODE
       module Admin
         class GenericController < ApplicationController
@@ -70,17 +94,17 @@ class CsrfSettingTest < Minitest::Test
     CODE
     code = <<-CODE
       module Admin
-        class PostsController < GenericController
+        class PostsController < Admin::GenericController
         end
       end
     CODE
     app = Spektr::App.new(checks: [Spektr::Checks::CsrfSetting])
-    app.rails_version = Gem::Version.new("4.0.0")
+    app.rails_version = Gem::Version.new('4.0.0')
     app.controllers = [
-      Spektr::Targets::Controller.new("application_controller.rb", application_controller),
-      Spektr::Targets::Controller.new("generic_controller.rb", generic_controller)
+      Spektr::Targets::Controller.new('application_controller.rb', application_controller),
+      Spektr::Targets::Controller.new('generic_controller.rb', generic_controller)
     ]
-    controller = Spektr::Targets::Controller.new("posts_controller.rb", code)
+    controller = Spektr::Targets::Controller.new('posts_controller.rb', code)
     check = Spektr::Checks::CsrfSetting.new(app, controller)
     check.run
     assert_equal 0, app.warnings.size
@@ -98,12 +122,11 @@ class CsrfSettingTest < Minitest::Test
      end
     CODE
     app = Spektr::App.new(checks: [Spektr::Checks::CsrfSetting])
-    app.rails_version = Gem::Version.new("4.0.0")
-    app.controllers = [Spektr::Targets::Controller.new("application_controller.rb", application_controller)]
-    controller = Spektr::Targets::Controller.new("posts_controller.rb", code)
+    app.rails_version = Gem::Version.new('4.0.0')
+    app.controllers = [Spektr::Targets::Controller.new('application_controller.rb', application_controller)]
+    controller = Spektr::Targets::Controller.new('posts_controller.rb', code)
     check = Spektr::Checks::CsrfSetting.new(app, controller)
     check.run
     assert_equal 1, app.warnings.size
   end
-
 end
