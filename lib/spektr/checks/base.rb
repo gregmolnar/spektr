fdf42c2224ef270de5aa099eaf5ea7ea4dfcf495
Greg Molnar <molnargerg@gmail.com>
2026-02-25 20:56:50 -0100

    fix parsing issues

--- lib/spektr/checks/base.rb
+++ lib/spektr/checks/base.rb
@@ -54,11 +54,11 @@ module Spektr
             return true if user_input?(argument)
           end
         end
-      when :embedded_statements_node, :if_node, :else_node
+      when :embedded_statements_node, :if_node, :else_node, :case_node, :embedded_variable_node
         node.statements.body.each do |item|
           return true if user_input? item
         end
-      when :interpolated_string_node, :interpolated_x_string_node, :interpolated_symbol_node
+      when :interpolated_string_node, :interpolated_x_string_node, :interpolated_symbol_node, :interpolated_regular_expression_node
         node.parts.each do |part|
           return true if user_input?(part)
         end
@@ -93,32 +93,79 @@ module Spektr
         end
         return if variable && variable.location.start_line == node.location.start_line
         return user_input?(variable)
-      when :local_variable_or_write_node
+      when :instance_variable_or_write_node, :local_variable_or_write_node
         return user_input?(node.value)
+      when :instance_variable_write_node, :local_variable_write_node
+        return user_input? node.value
       when :and_node, :or_node
         return user_input?(node.left)
         return user_input?(node.right)
-      when :instance_variable_write_node, :local_variable_write_node
-        return user_input? node.value
       when :splat_node
         return user_input? node.expression
       when :parentheses_node
         node.body.body.each do |item|
           return user_input? item
         end
-      when :string_node, :symbol_node, :constant_read_node, :integer_node, :true_node, :constant_path_node, :nil_node, :true_node, :false_node, :self_node, :global_variable_read_node
+      when :string_node, :symbol_node, :constant_read_node, :integer_node, :true_node, :constant_path_node, :nil_node, :true_node, :false_node, :self_node, :global_variable_read_node, :and_node
         # do nothing
       else
-        raise "Unknown argument type #{node.type.inspect} #{node.inspect}"
+        Spektr::Logger.debug "Unknown argument type #{node.type.inspect} #{node.inspect}"
       end
       false
     end
 
     # TODO: this doesn't work properly
     def model_attribute?(node)
+      return false if node.nil?
       model_names = @app.models.collect(&:name)
       case node.type
-      when :local_variable_read_node, :instance_variable_read_node
+      when :call_node
+        return model_attribute?(node.receiver) if node.receiver
+        if node.arguments
+          node.arguments.arguments.each do |argument|
+            return true if model_attribute?(argument)
+          end
+        end
+      when :embedded_statements_node, :if_node, :else_node, :case_node, :embedded_variable_node
+        node.statements.body.each do |item|
+          return true if model_attribute? item
+        end
+      when :interpolated_string_node, :interpolated_x_string_node, :interpolated_symbol_node, :interpolated_regular_expression_node
+        node.parts.each do |part|
+          return true if model_attribute?(part)
+        end
+      when :keyword_hash_node, :hash_node
+        node.elements.each do |element|
+          return true if model_attribute?(element.key)
+          return true if model_attribute?(element.value)
+        end
+      when :array_node
+        node.elements.each do |element|
+          return true if model_attribute?(element)
+        end
+      # TODO: make this better. ivars can be overridden in the view as well and
+      # can be set in non controller targets too
+      when :_instance_variable_read_node
+        return false unless @target.respond_to?(:view_path)
+        actions = []
+        @app.controllers.each do |controller|
+          actions = actions.concat controller.actions.select { |action|
+            action.template == @target.view_path
+          }
+        end
+        actions.each do |action|
+          next unless action.body
+          action.body.each do |exp|
+            return true if exp.name == node.name && model_attribute?(exp)
+          end
+        end
+      when :local_variable_read_node
+        variable = @target.lvars.find do |n|
+          n.name == node.name
+        end
+        return if variable && variable.location.start_line == node.location.start_line
+        return model_attribute?(variable)
+      when :instance_variable_read_node
         # TODO: handle helpers here too
         if ["Spektr::Targets::Controller", "Spektr::Targets::View"].include?(@target.class.name)
           actions = []
@@ -134,53 +181,25 @@ module Spektr
             end
           end
         end
-      when :local_variable_or_write_node
+      when :instance_variable_or_write, :local_variable_or_write_node
         return model_attribute?(node.value)
+      when :instance_variable_write_node, :local_variable_write_node
+        return model_attribute? node.value
       when :and_node, :or_node
         return model_attribute?(node.left)
         return model_attribute?(node.right)
-      when :call_node
-        return model_attribute?(node.receiver) if node.receiver
-        if node.arguments
-          node.arguments.arguments.each do |argument|
-            return true if model_attribute?(argument)
-          end
-        end
-      when :keyword_hash_node, :hash_node
-        node.elements.each do |element|
-          return true if model_attribute?(element.key)
-          return true if model_attribute?(element.value)
-        end
-      when :array_node
-        node.elements.each do |element|
-          return true if model_attribute?(element)
-        end
+      when :splat_node
+        return model_attribute? node.expression
       when :parentheses_node
         node.body.body.each do |item|
           return model_attribute? item
         end
-      when :interpolated_string_node, :interpolated_x_string_node, :interpolated_symbol_node
-        node.parts.each do |part|
-          return true if model_attribute?(part)
-        end
-      when :embedded_statements_node, :if_node, :else_node
-        node.statements.body.each do |item|
-          return true if model_attribute? item
-        end
-      when :instance_variable_write_node, :local_variable_write_node
-        return model_attribute? node.value
       when :constant_read_node
         return true if model_names.include? node.name.to_s
-      when :interpolated_string_node
-        node.parts.each do |item|
-          return model_attribute? item
-        end
-      when :splat_node
-        return model_attribute? node.expression
       when :string_node, :symbol_node, :integer_node, :constant_path_node, :nil_node, :true_node, :false_node, :self_node, :global_variable_read_node
         # do nothing
       else
-        raise "Unknown argument type #{node.type}"
+        Spektr::Logger.debug "Unknown argument type #{node.type}"
       end
     end
 
